9af90b10001282595c749c546d0ad3fc805d0d6e
HUGE | Martin Olson <molson@molson-ny-01823.ad.hugeinc.com>
2014-04-16 17:25:54 -0400

    Cleaned up the main code.

--- readomator-rysnc.sh
+++ readomator-rysnc.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+#load configs
 . config.sh
 
+#get the RSS feed and all of its enclosures
 wget -q -O- $SRC_FEED | grep -o '<enclosure url="[^"]*' | grep -o '[^"]*$' | xargs wget -c --directory-prefix=out
 wget -q -O- $SRC_FEED  > out/index.xml
 
-src="${SRC_FEED//\//\\/}"
-dest="${DEST_FEED//\//\\/}"
-sed -i '' "s/$src/$dest/g" out/index.xml
+#update the paths in the RSS feed
+sed -i '' "s/${SRC_FEED//\//\\/}/${DEST_FEED//\//\\/}/g" out/index.xml
 
+#rsync the goodies to its new home
 rsync -avz out/ $DEST_RSYNC
 
+# inform the user things are done
 echo "Readomator Podcast Downloaded & Rsync'd.  Your podcast is available at $DEST_FEED/index.xml"
