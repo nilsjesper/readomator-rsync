3c6ea68387072cc3c749641c737291efa26deaa4
HUGE | Martin Olson <molson@molson-ny-01823.ad.hugeinc.com>
2014-04-17 18:31:34 -0400

    Added mp3 conversion, better messaging and other improvements.

--- readomator-rysnc.sh
+++ readomator-rysnc.sh
@@ -3,15 +3,57 @@
 #load configs
 . config.sh
 
+echo "--------------------------"
+echo "Downloading Podcast Audio & Feed"
+echo "--------------------------"
+
 #get the RSS feed and all of its enclosures
-wget -q -O- $SRC_FEED | grep -o '<enclosure url="[^"]*' | grep -o '[^"]*$' | xargs wget -c --directory-prefix=out
+wget -q -O- $SRC_FEED | grep -o '<enclosure url="[^"]*' | grep -o '[^"]*$' | xargs wget -nc -T 15 -c --directory-prefix=out
 wget -q -O- $SRC_FEED  > out/index.xml
+
+#convert the files to mp3
+echo "--------------------------"
+echo "Converting Audio to MP3 (this may take a minute)"
+echo "--------------------------"
+
+if [ "$(ls -A out/*.aiff)" ]; then
+
+    cd out
+    for i in *.aiff;
+      do name=`echo $i | cut -d'.' -f1`;
+      echo "Converting $i..."
+      ffmpeg -y -v error -i $i -b 192k $name.mp3
+
+      rm $i
+    done
+    cd ../
+
+else
+    echo "There were no generated aiff files. Check your settings."
+    exit
+fi
+
+echo "--------------------------"
+echo "Changing paths in Feed"
+echo "--------------------------"
 
 #update the paths in the RSS feed
 sed -i '' "s/${SRC_FEED//\//\\/}/${DEST_FEED//\//\\/}/g" out/index.xml
 
+#change aiff references to mp3 references.
+sed -i '' "s/\.aiff\"/.mp3\"/g" out/index.xml
+sed -i '' "s/\/aiff/\/mp3/g" out/index.xml
+
+echo "--------------------------"
+echo "Rscynch-ing files"
+echo "--------------------------"
+
 #rsync the goodies to its new home
-rsync -avz out/ $DEST_RSYNC
+rsync  --progress -avz out/ $DEST_RSYNC
 
 # inform the user things are done
-echo "Readomator Podcast Downloaded & Rsync'd.  Your podcast is available at $DEST_FEED/index.xml"
+echo "--------------------------"
+echo "Done. Readomator Podcast Downloaded & Rsync'd. "
+echo " Your podcast is available at $DEST_FEED/index.xml"
+echo "--------------------------"
+
